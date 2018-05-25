c8ad91269c36257e7efca82dacea4a1553667335
Elton Lin <eltonlin1998@gmail.com>
2018-05-25 01:10:22 -0400

    finish model of MVC, setup view

---
+++ PlayingCard/PlayingCardView.swift
@@ -0,0 +1,13 @@
+//
+//  PlayingCardView.swift
+//  PlayingCard
+//
+//  Created by Elton Lin on 5/25/18.
+//  Copyright © 2018 MIT. All rights reserved.
+//
+
+import UIKit
+
+class PlayingCardView: UIView {
+
+}
