30a75fe3c0183c21d030f1fc86e50f13007b39d9
Elton Lin <eltonlin1998@gmail.com>
2018-05-28 09:43:10 +0100

    create getAttributedString() (for the card corners)

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class PlayingCardView: UIView {
     
+    private func centeredAttributedString(string: String, fontSize: CGFloat) -> NSAttributedString {
+        var font = UIFont.preferredFont(forTextStyle: .body).withSize(fontSize)
+        font = UIFontMetrics(forTextStyle: .body).scaledFont(for: font)
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .center
+        return NSAttributedString(string: string, attributes: [.paragraphStyle: paragraphStyle, .font: font])
+    }
+    
     override func draw(_ rect: CGRect) {
         let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
         UIColor.white.setFill()
