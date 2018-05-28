88592727b53f43dbadb84b2ba163b0909260f414
Elton Lin <eltonlin1998@gmail.com>
2018-05-28 10:19:13 +0100

    Lecture 6, 30:19, store constants and computed properties in a struct

--- PlayingCard/PlayingCardView.swift
+++ PlayingCard/PlayingCardView.swift
@@ -15,7 +15,7 @@ class PlayingCardView: UIView {
     var isFaceUp = true { didSet { setNeedsDisplay() }}
     
     private var corneredString: NSAttributedString {
-        return centeredAttributedString(string: "9" + "\n" + "♥︎", fontSize: 16.0)
+        return centeredAttributedString(string: rankString + "\n" + suit, fontSize: cornerFontSize)
     }
     
     private func centeredAttributedString(string: String, fontSize: CGFloat) -> NSAttributedString {
@@ -27,8 +27,41 @@ class PlayingCardView: UIView {
     }
     
     override func draw(_ rect: CGRect) {
-        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: 16.0)
+        let roundedRect = UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
         UIColor.white.setFill()
         roundedRect.fill()
+    }
+}
+
+extension PlayingCardView {
+    private struct SizeRatio {
+        static let cornerFontSizeToBoundsHeight: CGFloat = 0.085
+        static let cornerRadiusToBoundsHeight: CGFloat = 0.06
+        static let cornerOffsetToCornerRadius: CGFloat = 0.33
+        static let faceCardImageSizeToBoundsSize: CGFloat = 0.75
+    }
+    
+    // computed properties that are derived from the constants
+    private var cornerRadius: CGFloat {
+        return bounds.size.height * SizeRatio.cornerRadiusToBoundsHeight
+    }
+    
+    private var cornerOffset: CGFloat {
+        return cornerRadius * SizeRatio.cornerOffsetToCornerRadius
+    }
+    
+    private var cornerFontSize: CGFloat {
+        return bounds.size.height * SizeRatio.cornerFontSizeToBoundsHeight
+    }
+    
+    private var rankString: String {
+        switch rank {
+        case 1: return "A"
+        case 2...10: return String(rank)
+        case 11: return "J"
+        case 12: return "Q"
+        case 13: return "K"
+        default: return "?"
+        }
     }
 }
